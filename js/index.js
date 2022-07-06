1b517a3830cdd19e238a297b1e9e416874648e48
Dolores Garcia <garcia.loli2001@gmail.com>
2022-07-05 23:14:37 -0300

    carrusel roto y sass roto

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.artistas__cards'), {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        arrows: {
+          prev: '.prev',
+          next: '.next'
+        }
+      });
+  })

33a61da45d0c2edac8f04d6553c0dbb666e4f025
Dolores Garcia <garcia.loli2001@gmail.com>
2022-07-17 09:49:41 -0300

    errores resultos, otros no. Comienzo a pasar todo a sass

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('load' , function(){
     new Glider(document.querySelector('.artistas__cards'), {
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -7,5 +7,5 @@ window.addEventListener('load', function(){
           prev: '.prev',
           next: '.next'
         }
-      });
+      })
   })
